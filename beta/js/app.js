255f548fb46b0acb39e9186972664b026228e083
Hamdan Latief <44519762+hamdanltf@users.noreply.github.com>
2021-02-10 15:09:43 +0700

    total redesign using pure css

---
+++ beta/js/app.js
@@ -0,0 +1,36 @@
+const eventsContainer = document.querySelector('.events-container')
+const nav = document.querySelector('nav')
+const welcomeEvent = document.querySelector('.welcome-event')
+const form = document.querySelector('.form')
+
+const showEvents = (event) => {
+  const { name, phone, email, description } = event
+
+  const output = `
+        <div class="card">
+          <div class="card--details">
+              <h1>${name}</h1>
+              <p>phone: ${phone}</p>
+              <p>email: ${email}</p>
+             <p>${description}</p>
+          </div>
+        </div>
+        `
+  eventsContainer.innerHTML += output;
+}
+
+const showLatestEvent = (latestEvent) => {
+
+  const { name, phone, email, description } = latestEvent
+  // Get the first event
+  welcomeEvent.innerHTML = `
+    <h1>${name}</h1>
+      <p>phone: ${phone}</p>
+      <p>email: ${email}</p>    
+    <p>${description.length >= 100 ? `${description.substring(0, 100)}...` : description}</p>`
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  addNewEvent()
+})
